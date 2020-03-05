7165f8d482abc7c1186b6881e47279bb00c79dfe
Neilfoster <Neilfoster81@gmail.com>
2020-03-05 11:45:50 +0000

    Added else statement to cart/views.py

--- cart/views.py
+++ cart/views.py
@@ -10,8 +10,11 @@ def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
 
-    cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+     cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
 
     request.session['cart'] = cart
     return redirect(reverse('index'))

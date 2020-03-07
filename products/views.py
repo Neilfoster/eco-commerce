22c7d2bd30506045b6e11e0ad53fc148e53ba818
Neilfoster <Neilfoster81@gmail.com>
2020-03-07 17:39:40 +0000

    Added spacing between functions

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Product
 # Create your views here.
 
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products/products.html", {"products": products})
@@ -9,8 +10,9 @@ def all_products(request):
 
 def all_household(request):
     products = Product.objects.filter(category='Household')
-    return render(request,"products/all_household.html", {'products': products})
+    return render(request, "products/all_household.html", {'products': products})
+
 
 def all_selfcare(request):
     products = Product.objects.filter(category='Selfcare')
-    return render(request,"products/all_selfcare.html", {'products': products})
+    return render(request, "products/all_selfcare.html", {'products': products})

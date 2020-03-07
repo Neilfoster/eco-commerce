94e0f3d9614709f2e55f0183c14d75e78204182c
Neilfoster <Neilfoster81@gmail.com>
2020-03-07 17:37:13 +0000

    chaged typos

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_products, all_household
+from .views import all_products, all_household, all_selfcare
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^all_household/$', all_household, name='all_household'),
+    url(r'^all_selfcare/$', all_selfcare, name='all_selfcare'),
 ]
--- products/views.py
+++ products/views.py
@@ -10,3 +10,7 @@ def all_products(request):
 def all_household(request):
     products = Product.objects.filter(category='Household')
     return render(request,"products/all_household.html", {'products': products})
+
+def all_selfcare(request):
+    products = Product.objects.filter(category='Selfcare')
+    return render(request,"products/all_selfcare.html", {'products': products})

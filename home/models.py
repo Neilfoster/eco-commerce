0875125ab83d17d9229f313fbca425443802032d
Neilfoster <Neilfoster81@gmail.com>
2020-03-19 15:27:59 +0000

    Added render import

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.shortcuts import render
 
 # Create your models here.
+
+
 def index(request):
-    """"Displays the index page """
+    """ Displays the index page """
     return render(request, "index.html")

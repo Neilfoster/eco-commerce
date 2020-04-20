ac9c710750194644fba3607364cd744263fc5e63
Neilfoster <Neilfoster81@gmail.com>
2020-04-20 21:43:21 +0000

    Added order.user = request.user to checkout statement

--- checkout/models.py
+++ checkout/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 from accounts.models import Profile
 from products.models import Product
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Order(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     full_name = models.CharField(max_length=70, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=40, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
-    street_address1 = models.CharField(max_length=40, blank= False)
-    street_address2 = models.CharField(max_length=40, blank= False)
-    county = models.CharField(max_length=40, blank= False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
     
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False)

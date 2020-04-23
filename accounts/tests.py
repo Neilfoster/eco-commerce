58eb4f409d1e2c38bfacffc0d914e257807f04fd
Neilfoster <Neilfoster81@gmail.com>
2020-04-23 12:19:07 +0000

    Added new testcase to accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from accounts.forms import UserRegistrationForm, UserLoginForm
 
-# Create your tests here.
+class AccountsFormsTests(TestCase):
+
+    def test_valid_user_registration_form(self):
+        form = UserRegistrationForm({
+            'username': 'mike-d',
+            'email': 'mikedhiggins@test.com',
+            'password1': 'wizard1',
+            'password2': 'wizard1'
+        })
+
+        self.assertTrue(form.is_valid())
+    
+    
+    def test_login_template(self):
+        response = self.client.get("/accounts/login", follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('login.html')

6d2d9d00b919a61564dc736c366e202180cfdbcf
Neilfoster <Neilfoster81@gmail.com>
2020-04-29 19:53:06 +0000

    changed input field for author of blog

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Blog(models.Model):
     image = models.ImageField(upload_to='images')
     headline = models.CharField(max_length=50, default='')
-    author = models.ForeignKey(User, default=None)
+    author = models.CharField(max_length=50, default='')
     blog_text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
